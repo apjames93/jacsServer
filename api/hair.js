5d09ff3830bced0343e48c395211225a5f048c56
Alexander James <apjames93@gmail.com>
2016-10-20 10:39:09 -0600

    seeting up server

--- api/hair.js
+++ api/hair.js
@@ -2,6 +2,4 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 var queries = require('../queries/apiQueries');
-var hair = require('./hair');
-router.use('/hair', hair);
 module.exports = router;
---
+++ queries/apiQueries.js
@@ -0,0 +1,3 @@
+var knex = require('../db/knex');
+module.exports ={
+};
