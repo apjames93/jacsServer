318f547cd2a3d718406101ed9e1f42355e46ffa1
Alexander James <apjames93@gmail.com>
2016-10-20 10:54:39 -0600

    setting up routes

--- api/hair.js
+++ api/hair.js
@@ -2,4 +2,34 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 var queries = require('../queries/apiQueries');
+var helpers =  require('../auth/helpers');
+
+
+
+router.get('/', function(req, res, next){
+
+});
+
+router.get('/', function(req, res, next){
+
+
+});
+
+router.get('/:id', function(req, res, next){
+
+});
+
+router.delete('/',helpers.ensureauthenticated,  function(req, res, next){
+
+});
+
+router.post('/',helpers.ensureauthenticated, function(req, res,next){
+
+});
+
+router.put('/',helpers.ensureauthenticated, function(req, res,next){
+
+});
+
+
 module.exports = router;
