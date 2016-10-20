0db5308bd6878a0b093b48444bf139c9c9261ced
Alexander James <apjames93@gmail.com>
2016-10-20 16:20:55 -0600

    added in the rest of the routes

--- api/hair.js
+++ api/hair.js
@@ -6,22 +6,48 @@ var helpers =  require('../auth/helpers');
 
 
 
-  router.get('/', function(req, res, next){
-    queries.getData()
-    .then(function(data){
-      res.json({test: 'data'});
-      });
-    });
-
-router.get('/:id', function(req, res, next){
+router.get('/', function(req, res, next){
   queries.getData()
   .then(function(data){
-    res.json({test: 'data'});
+    res.json({allData: data});
   });
 });
 
+router.get('/:id', function(req, res, next){
+  queries.getcat(req.params.id)
+  .then(function(data){
+    res.json({catData: data});
+  });
+});
 
+router.get('/work/:id', function(req, res, next){
+  queries.getPic(req.params.id)
+  .then(function(data){
+    res.json({test: data});
+  });
+});
 
+router.post('/', function(req, res, next ){
+  queries.addData(req.query)
+  .then(function(data){
+    res.json({data : data});
+  });
+});
+
+router.delete('/:id',  function(req, res, next){
+  queries.deleteData(req.query.id)
+  .then(function(data){
+    res.json({data : data});
+  });
+});
+
+router.put('/',  function(req, res, next){
+  console.log(req.query);
+  queries.editData(req.query)
+  .then(function(data){
+    res.json({data : data});
+  });
+});
 
 
 
--- queries/apiQueries.js
+++ queries/apiQueries.js
@@ -2,7 +2,43 @@ var knex = require('../db/knex');
 module.exports ={
 
   getData : function(){
-    return knex('work');
+    return knex('work').innerJoin('category', 'lookUp', 'category.id');
   },
+
+  getcat : function(id){
+    return knex('work').innerJoin('category', 'lookUp', 'category.id').where({lookUp: id});
+  },
+
+  getPic : function(id){
+    return knex('work')
+    .where({id: id});
+  },
+
+  addData : function(data){
+    return knex('work')
+    .insert({
+      description: data.description,
+      img: data.img,
+      lookUp : data.lookUp
+    });
+  },
+
+  editData : function(data){
+    console.log(data);
+    return knex('work')
+    .update({
+      description: data.description,
+      img: data.img,
+      lookUp: data.lookUp
+    })
+    .where({id : data.id});
+  },
+
+  deleteData : function(data){
+    return knex('work')
+    .where({id: data})
+    .del();
+  }
+
 
 };
